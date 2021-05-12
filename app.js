6a74c98be0604666118c7f6e968d6cfa0cf91263
RobertoBarbagallo <roberto-barbagallo@live.it>
2021-05-12 09:12:48 +0200

    spazi

--- app.js
+++ app.js
@@ -6,48 +6,38 @@ new Vue({
         eMailList: [],
         times: "",
         tempList: [],
-        loading: false 
+        loading: false
 
     },
 
     methods: {
 
         eMailGenerator(times) {
-
-            const secTempList = []
-            this.loading = true 
+            const secTempList = [];
+            this.loading = true;
 
             for (let index = 0; index < times; index++) {
                 axios.get(' https://flynn.boolean.careers/exercises/api/random/mail').then((resp) => {
 
                     secTempList.push(resp.data.response);
 
-                    if(secTempList.length == times){
-                        this.tempList = secTempList
-                        this.render()
-
+                    if (secTempList.length == times) {
+                        this.tempList = secTempList;
+                        this.render();
                     }
- 
                 });
-
             }
-   
-
         },
 
-        render(){
-
-                this.eMailList= this.tempList
-                console.log(this.eMailList)
-                this.loading = false
-            
+        render() {
+            this.eMailList = this.tempList;
+            console.log(this.eMailList);
+            this.loading = false;
         }
-
     },
 
     mounted() {
 
 
     },
-
 })
